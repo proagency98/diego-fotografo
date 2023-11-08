c1e9cf645c149e3b61c2092327d47375d03b385f
Luis Flores <frko.lc@MacBook-Pro-de-Luis.local>
2023-11-08 15:55:11 +0100

    añadimos nuevo componente, ya que el anterior se ha eliminado

---
+++ src/app/videos-diego/videos-diego.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-videos-diego',
+  templateUrl: './videos-diego.component.html',
+  styleUrls: ['./videos-diego.component.css']
+})
+export class VideosDiegoComponent {
+
+}
